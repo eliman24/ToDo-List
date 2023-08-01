65cd29c02a3faf7174735322e66eeadd9ddae81a
Elisabeth Manalo <eliman24@bergen.org>
2023-08-01 09:38:15 -0400

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
